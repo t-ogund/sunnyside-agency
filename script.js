11599fb921d5aa982f4583bc8d56297efe84edc7
Toye Ogundepo <ogundepo@gmail.com>
2022-08-09 20:21:25 -0500

    enhance ui.

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 const hamburger = document.querySelector('#hamburger');
 const hamburgerMenu = document.querySelector('#hamburger-menu');
-const transformLearnMore = document.querySelector('#transform-learn-more');
-const transformUnderline = document.querySelector('#transform-underline');
-const standoutLearnMore = document.querySelector('#stand-out-learn-more');
-const standoutUnderline = document.querySelector('#stand-out-underline');
 
 hamburger.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('hide');
-})
-
-transformLearnMore.addEventListener('mouseenter', () => {
-    transformUnderline.style.display = 'block';
-})
-
-standoutLearnMore.addEventListener('mouseenter', () => {
-    standoutUnderline.style.display = 'block';
-})
-
-standoutLearnMore.addEventListener('mouseleave', () => {
-    standoutUnderline.style.display = 'none';
 })
